0cd28e7cd55667ecbe3e3af3ba2ed11ad06c81a0
AgbajiAlexander <alexanderogah@gmail.com>
2026-04-09 11:14:59 +0100

    learners commit

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// arry := [3]int {0:12,1:13}
+	// var cars = [6]string {"benz", "honda", "mazda", "gis", "toyota", "inoson"}
+	// goLang := []string{}
+
+	// cars [3] = "cybertruck"
+
+	// fmt.Println(cars)
+	// fmt.Println(cars[5])
+	// fmt.Println(cars[3])
+	// fmt.Println(arry)
+	// fmt.Println(len(cars))
+	// fmt.Println(len(arry))
+	// fmt.Println(len(goLang))
+	// fmt.Println(cap(goLang))
+
+	// price := []int{12,5,8}
+	// price1 := []int{67,68,75}
+	// price2 := append(price, price1...)
+
+	// fmt.Printf("the slice will be = %v\n", price2)
+	// fmt.Printf("the length of the new slice is = %d\n", len(price2))
+	// fmt.Printf("the capacity of the new slice is = %d\n", cap(price2))
+
+// 	newArray := [13]int{1,2,3,4,4,5,5,6,6,6,6,2,4}
+// 	newSlice := newArray[4:12]
+
+// 	fmt.Printf("new sclice = %v\n", newSlice)
+// 	fmt.Printf("length of new slice = %d\n", len(newSlice))
+// 	fmt.Printf("capacity of the new slice = %d\n", cap(newSlice))
+// fmt.Println()
+// newSlice = newArray[1:4] // Change length by re-slicing the array
+//   	fmt.Printf("the re sliced = %v\n", newSlice)
+//   	fmt.Printf("length = %d\n", len(newSlice))
+//   	fmt.Printf("capacity = %d\n", cap(newSlice))
+// fmt.Println()
+// newSlice = append(newSlice, 20, 21, 22, 23) // Change length by appending items
+//   fmt.Printf("myslice1 = %v\n", newSlice)
+//   fmt.Printf("length = %d\n", len(newSlice))
+//   fmt.Printf("capacity = %d\n", cap(newSlice))
+
+familyMembers := 11
+
+switch familyMembers {
+case 1:
+	fmt.Println ("Godswill is the first Son")
+case 2:
+	fmt.Println ("Alexander is the Second Son")
+case 3:
+	fmt.Println ("Edwin is the third Son")
+case 4:
+	fmt.Println ("Ruth is the First Daughter")
+case 5:
+	fmt.Println ("Helen is the second daughter")
+case 6: 
+	fmt.Println ("Innocent is the fourth Son")
+case 7:
+	fmt.Println ("Gideon is the fifth Son")
+case 8:
+	fmt.Println("Aaron is the sixth Son")
+case 9:
+fmt.Println("Blessing is the third Daughter")
+case 10:
+	fmt.Println("Ezekiel is the seventh Son")
+default:
+	fmt.Println("Is not a member of the family")
+}
+}
