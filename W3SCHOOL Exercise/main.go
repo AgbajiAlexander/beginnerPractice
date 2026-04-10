9db4821d317efeadb0ad546e7efa46c1ae778a05
AgbajiAlexander <alexanderogah@gmail.com>
2026-04-10 17:21:43 +0100

    working on swithces

--- W3SCHOOL Exercise/main.go
+++ W3SCHOOL Exercise/main.go
@@ -73,16 +73,16 @@ func main() {
 	// }
 
 	//working with multiple switch cases
-	familyMembers := 10
+	// familyMembers := 10
 
-	switch familyMembers {
-	case 1, 2, 3, 6, 7, 8, 10:
-		fmt.Println("One of the boys in the family")
-	case 4, 5, 9:
-		fmt.Println("One the girls in the family")
-	default:
-		fmt.Println("Is not a member of the family")
-	}
+	// switch familyMembers {
+	// case 1, 2, 3, 6, 7, 8, 10:
+	// 	fmt.Println("One of the boys in the family")
+	// case 4, 5, 9:
+	// 	fmt.Println("One the girls in the family")
+	// default:
+	// 	fmt.Println("Is not a member of the family")
+	// }
 
 	// working with loops
 	// for i := 0; i < 10; i++ {
@@ -96,10 +96,49 @@ func main() {
 	// 	fmt.Printf("the value of i is %d\n", i)
 	// }
 
+	// for i := 0; i < 10; i++ {
+	// 	if i == 5 {
+	// 		continue
+	// 	}
+	// 	fmt.Printf("the value of i is %d\n", i)
+	// }
+
+	//the break statement: is used to exit a loop when a certain condition is met, while the continue statement is used to skip the current iteration of a loop and move on to the next one. In the example above, the break statement will stop the loop when i equals 5, while the continue statement will skip the iteration when i equals 5 and continue with the next iteration.
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 == 0 {
+	// 		continue
+	// 	}
+	// 	fmt.Printf("the value of i is %d\n", i)
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Printf("the value of i is %d\n", i)
+	// 	}
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 != 0 {
+	// 		fmt.Printf("the value of i is %d\n", i)
+	// 	}
+	// }
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 == 0 {
+	// 		fmt.Printf("the value of i is %d\n", i)
+	// 	}
+	// }
+
+	// for i := 0; i < 10; i++ {
+	// 	if i%2 != 0 {
+	// 		fmt.Printf("the value of i is %d\n", i)
+	// 	}
+	// }
+
 	for i := 0; i < 10; i++ {
-		if i == 5 {
-			continue
+		if i == 3 {
+			fmt.Printf("the value of i is %d\n", i)
+		} else {
+			fmt.Printf("the value of i is %d\n", i)
 		}
-		fmt.Printf("the value of i is %d\n", i)
 	}
 }
